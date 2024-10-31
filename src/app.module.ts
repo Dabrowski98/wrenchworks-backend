6821bf1ca27f1f1b31106f58b74abcf0f4cc8fa8
Dabrowski98 <dabrowski.konrad.it@gmail.com>
2024-10-31 13:34:16 +0100

    - configured error format returned to backend user. - added logger in app.module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
-import { DatabaseService } from './database/database.service';
+import { Logger, Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './modules/users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AddressModule } from './modules/index';
+import { AppController } from './app.controller';
+import { HelperModule } from './common/helper.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
-  providers: [DatabaseService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+
+      formatError: (error) => {
+        let originalError = error.extensions?.originalError as
+          | Error
+          | undefined;
+        return {
+          message: 
+          originalError ? originalError.message : 
+          error.message,
+          code: error.extensions.code,
+          status: error.extensions.status,
+        };
+      },
+    }),
+
+    DatabaseModule,
+    HelperModule,
+    AddressModule,
+  ],
+  providers: [
+    Logger,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
